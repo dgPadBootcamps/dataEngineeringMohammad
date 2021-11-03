6337d2948fd4cb5657110a3f7b183c26de017b7c
Mohammed Haidar <mohammed.haidar.1999@hotmail.com>
2021-11-03 18:14:01 +0200

    Scraped into csv

--- Test.py
+++ Test.py
@@ -1,13 +1,23 @@
 import twitter as tw
+import csv
 
 item = tw.TwitterSearchScraper("corona lebanon", False)
 
-tweets = []
+tweets = []  # the list of the data
 
 for i, tweet in enumerate(item.get_items()):
     if i > 10:
         break
 
-    tweets.append([tweet.id, tweet.user.username, tweet.content, tweet.likeCount])
+    tweets.append([tweet.id, tweet.user.username, tweet.content, tweet.likeCount])  # the props of the data I need
 
-print(tweets)
+header = [' TweetId', 'UserName', 'Content', 'LikesNumber']  # The header of the data props
+
+# open the file in the write mode
+with open('CoronaLebanon.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)  # the csv writer
+
+    # write the header
+    writer.writerow(header)
+    # write the fetched data
+    writer.writerows(tweets)
