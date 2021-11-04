6bf82ad86214c02f93215348dcb03bdf4b15fbd7
Mohammed Haidar <mohammed.haidar.1999@hotmail.com>
2021-11-04 20:48:07 +0200

    Add variable to the URL

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, jsonify
-from Test import tw
+from Test import read_tweets
+from Test import save_tweets
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def tweets():
-    return jsonify(tw())
+def hello():
+    return "Hello"
+
+
+@app.route('/tweets/<search>')
+def tweets(search):
+    return jsonify(read_tweets(search))
+
+
+@app.route('/save/<search>')
+def save(search):
+    return jsonify(save_tweets(search))
 
 
 app.run(debug=True)
