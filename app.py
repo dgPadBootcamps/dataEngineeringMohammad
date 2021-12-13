752235bdc401e21e7c21b5699e46c2a24be35b60
Mohammed Haidar <mohammed.haidar.1999@hotmail.com>
2021-12-13 18:37:42 +0200

    app

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify
-from Test import read_tweets
-from Test import save_tweets
+import json
+
+from flask import Flask, jsonify, render_template
+from data_collection.collection import search
+from data_collection.get_hashtags import get_hashtags
+from datetime import datetime, timedelta
+
+today = datetime.utcnow().date()  # to concatenate in the file name the date of today
+yesterday = today - timedelta(days=1)
 
 app = Flask(__name__)
 
@@ -10,14 +16,14 @@ def hello():
     return "Hello"
 
 
-@app.route('/tweets/<search>')
-def tweets(search):
-    return jsonify(read_tweets(search))
+@app.route('/tweets/<tweet>')
+def tweets(tweet):
+    return jsonify(search(tweet, yesterday, today))
 
 
-@app.route('/save/<search>')
-def save(search):
-    return jsonify(save_tweets(search))
+@app.route('/twitter/taganalysis/<word>')
+def top_hashtags(word):
+    return jsonify(get_hashtags(word))
 
 
 app.run(debug=True)
